6b9f1ebddb373eafc63e4afecfc429e0aadf45e8
Jojoyel <jolalle974@gmail.com>
2022-04-11 11:58:14 +0200

    Added map

--- app/src/main/java/com/alterneo/alterneo_app/MainActivity.java
+++ app/src/main/java/com/alterneo/alterneo_app/MainActivity.java
@@ -1,8 +1,11 @@
 package com.alterneo.alterneo_app;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.mapbox.maps.MapView;
+import com.mapbox.maps.Style;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		MapView mapView = findViewById(R.id.mapView);
+		mapView.getMapboxMap().loadStyleUri(Style.MAPBOX_STREETS);
 	}
 }
